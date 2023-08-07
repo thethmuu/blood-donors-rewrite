2832b7c7fe0a5a4fe40dc5c8873e4b6f5b73af66
aungchanmyaethaw <aungchanmyaethaw0610@gmail.com>
2023-08-07 10:16:08 +0630

    project initial setup

---
+++ server/src/libs/prisma.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const globalForPrisma = global;
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
