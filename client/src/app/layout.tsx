f631b349f1f9076d4463e1275edae0f0e2b17b62
nyominkhat <108140828+nyominkhat@users.noreply.github.com>
2023-08-11 20:02:13 +0630

    client ui fixed

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
-import "./globals.css";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className} suppressHydrationWarning={true}>
+      <body
+        className={`${inter.className} w-screen h-screen overflow-hidden`}
+        suppressHydrationWarning={true}
+      >
         {children}
       </body>
     </html>
