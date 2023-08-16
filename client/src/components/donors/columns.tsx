a7f59bfadf1b03c83692bbcb2fe45f456ffc0277
nyominkhat <108140828+nyominkhat@users.noreply.github.com>
2023-08-16 23:35:40 +0630

    donor detail page

--- client/src/components/donors/columns.tsx
+++ client/src/components/donors/columns.tsx
@@ -4,6 +4,7 @@ import { ColumnDef } from "@tanstack/react-table";
 import { ArrowUpDown } from "lucide-react";
 
 import { Donor } from "@/types/donor";
+import Link from "next/link";
 
 export const columns: ColumnDef<Donor>[] = [
   {
@@ -21,6 +22,18 @@ export const columns: ColumnDef<Donor>[] = [
           အမည်
           <ArrowUpDown className="w-4 h-4 ml-2" />
         </p>
+      );
+    },
+    cell: ({ row }) => {
+      const userId = row.original.id;
+
+      return (
+        <Link
+          href={`/donors/profile/${userId}`}
+          className="font-semibold transition-all text-primary hover:text-primary/80"
+        >
+          {row.original.name}
+        </Link>
       );
     },
   },
--- client/src/hooks/donors/useDonor.ts
+++ client/src/hooks/donors/useDonor.ts
@@ -1,10 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 
 import getDonor from "@/services/donors/getDonor";
-import { LargeNumberLike } from "crypto";
 
 const useDonor = (id: number) => {
-  useQuery(["donor"], () => getDonor(id));
+  return useQuery(["donor", id], () => getDonor(id));
 };
 
 export default useDonor;
