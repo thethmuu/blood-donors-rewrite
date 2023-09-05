ca7e608df7c73a85a94d908666a62bb1a623ad9b
aungchanmyaethaw <aungchanmyaethaw0610@gmail.com>
2023-09-05 23:27:33 +0630

    prisma lib file change

--- server/src/libs/prisma.ts
+++ server/src/libs/prisma.ts
@@ -4,10 +4,26 @@ declare global {
   var prisma: PrismaClient;
 }
 
-const globalForPrisma = global;
-
-const prisma = globalForPrisma.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 export default prisma;
+
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// const globalForPrisma = global;
+
+// const prisma = globalForPrisma.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
